b705e423b48e0ecfc91cf54a89e58167ec64a8ab
Ckirusha <115962585+Zopax@users.noreply.github.com>
2023-04-15 01:36:18 +0700

    15.04.2023

---
+++ OpenWeatherUI/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OpenWeatherUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Window takeTown = new TakeTown();
+            takeTown.Show();
+
+            if (takeTown == null)
+            {
+                InitializeComponent();
+            }
+        }
+
+        //public void GetData()
+        //{
+        //    var key = "11d6bb62d4cffe5e2e93a95d534cea5c";
+        //    var location = ;
+        //    var request = new GetRequest($"https://api.openweathermap.org/data/2.5/weather?lang=RU&q={location}&appid={key}");
+        //    request.Run();
+
+        //    var response = request.Response;
+        //}
+    }
+}
---
+++ OpenWeatherUI/TakeTown.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OpenWeatherUI
+{
+    /// <summary>
+    /// Interaction logic for TakeTown.xaml
+    /// </summary>
+    public partial class TakeTown : Window
+    {
+        public TakeTown()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseTab_Click ( object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            { 
+                this.DragMove(); 
+            }
+        }
+    }
+}
